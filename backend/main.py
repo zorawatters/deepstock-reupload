ee930806c80655faf11f7d5600595e72edf696ee
Oscar <oscardel413@gmail.com>
2020-03-15 19:17:24 +0000

    updated main,requirements. main now has def for adding tweets and is connect to mongodb

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import request
+from flask_pymongo import pymongo
+from pymongo import MongoClient
 import yfinance as yf
+
 app = Flask(__name__)
+
+CONNECTION_STRING = "mongodb://deep-stock-cu:deep2020stock@deep-stock-cluster-shard-00-00-bwk5a.gcp.mongodb.net:27017,deep-stock-cluster-shard-00-01-bwk5a.gcp.mongodb.net:27017,deep-stock-cluster-shard-00-02-bwk5a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=deep-stock-cluster-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('deep-stock')
+collection = db['companies']
+
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -13,5 +24,10 @@ def get_message():
 def get_stock_data():
 	msft = yf.Ticker("MSFT")
 	print(msft.info)
+
+@app.route("/<string:company>/tweets", methods=['PUT'])
+def addTweet(company):
+    new_tweet = request.get_json()
+    collection.update({"ticker" : company}, {'$push': {'tweets': new_tweet}})
 
 app.run(host='0.0.0.0')
