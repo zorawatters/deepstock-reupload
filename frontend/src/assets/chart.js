3de9a536f2ae9468f04fc92e670a3de913a3370d
Keaton Whitehead <laxsta@Keatons-MacBook-Pro.local>
2020-03-19 18:13:59 -0600

    Updateing files

--- frontend/src/assets/chart.js
+++ frontend/src/assets/chart.js
@@ -1,16 +1,14 @@
 //Loading data
 
-const loadData = d3.json("stockDataExample.json", function(data){console.log(data);});
-
-// .then(data => {
-//   const chartResultsData = data['chart']['result'][0];
-//   const quoteData = chartResultsData['indicators']['quote'][0];
-//   return chartResultsData['timestamp'].map((time, index) => ({
-//     date: new Date(time * 1000),
-//     high: quoteData['high'][index],
-//     low: quoteData['low'][index],
-//     open: quoteData['open'][index],
-//     close: quoteData['close'][index],
-//     volume: quoteData['volume'][index]
-//   }));
-// });
+const loadData = d3.json("/stockDataExample.json").then(data => {
+  const chartResultsData = data['chart']['result'][0];
+  const quoteData = chartResultsData['indicators']['quote'][0];
+  return chartResultsData['timestamp'].map((time, index) => ({
+    date: new Date(time * 1000),
+    high: quoteData['high'][index],
+    low: quoteData['low'][index],
+    open: quoteData['open'][index],
+    close: quoteData['close'][index],
+    volume: quoteData['volume'][index]
+  }));
+});
