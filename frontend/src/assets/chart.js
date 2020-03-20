f47675a35db29378846a6c1afc80f3632eafe3a9
Keaton Whitehead <laxsta@Keatons-MacBook-Pro.local>
2020-03-19 18:25:49 -0600

    Working json data in console.log

--- frontend/src/assets/chart.js
+++ frontend/src/assets/chart.js
@@ -1,14 +1,33 @@
 //Loading data
 
-const loadData = d3.json("/stockDataExample.json").then(data => {
-  const chartResultsData = data['chart']['result'][0];
-  const quoteData = chartResultsData['indicators']['quote'][0];
-  return chartResultsData['timestamp'].map((time, index) => ({
-    date: new Date(time * 1000),
-    high: quoteData['high'][index],
-    low: quoteData['low'][index],
-    open: quoteData['open'][index],
-    close: quoteData['close'][index],
-    volume: quoteData['volume'][index]
-  }));
+const loadData = d3.json("stockDataExample.json",function(data) {
+    // console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+       console.log(data[i].Volume);
+       console.log(data[i].High);
+   }
+
 });
+
+//   "Date":"2020-03-06T00:00:00.000Z",
+  // "Open":162.61,
+  // "High":163.11,
+  // "Low":156.0,
+  // "Close":161.57,
+  // "Volume":72790000,
+  // "Dividends":0,
+  // "Stock Splits":0
+
+// .then(data => {
+//   const chartResultsData = data['chart']['result'][0];
+//   const quoteData = chartResultsData['indicators']['quote'][0];
+//   return chartResultsData['timestamp'].map((time, index) => ({
+//     date: new Date(time * 1000),
+//     high: quoteData['high'][index],
+//     low: quoteData['low'][index],
+//     open: quoteData['open'][index],
+//     close: quoteData['close'][index],
+//     volume: quoteData['volume'][index]
+//   }));
+// });
