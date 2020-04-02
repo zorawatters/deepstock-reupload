40e6280d9171533a1a8c7484b4838ae5c02cd5a6
JakeAndrus <thejakeandrus@gmail.com>
2020-04-01 21:46:48 -0600

    new repo test

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,15 +4,16 @@ echo "We're runing deploy prod"
 
 set -e
 
-docker build -t gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
-docker build -t gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
+docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
+docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
 
 gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509.json
 
 gcloud --quiet config set project deep-stock-268818
 
-gcloud docker -- push gcr.io/deep-stock-268818/appengine
-# gcloud docker -- push gcr.io/deep-stock-268818/appengine/deep-stock-frontend
+# gcloud docker -- push us.gcr.io/deep-stock-268818/appengine
+gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-frontend
+gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-backend
 
-# yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
-# yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
+# yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
+# yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
