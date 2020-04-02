fef4082321bef351118e6b3023441dca659bc23a
Natnael Abraha <correct-natnael.abraha@colorado.edu>
2020-04-01 18:48:15 -0600

    added deploy-rad

---
+++ deploy-prod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+docker build -t gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile .
+docker build -t gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile .
+
+gcloud auth activate-service-account --key-file ${HOME}/deep-stock-268818-fce7b0e95509.json
+
+gcloud --quiet config set project deep-stock-268818
+
+gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-backend
+gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-frontend
+
+# yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
+# yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
