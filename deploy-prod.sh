150424fbd98706eceeee264336145255f90b7b96
Natnael Abraha <correct-natnael.abraha@colorado.edu>
2020-04-01 19:45:15 -0600

    removed home

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,12 +7,12 @@ set -e
 docker build -t gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
 docker build -t gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
 
-gcloud auth activate-service-account --key-file ${HOME}/deep-stock-268818-fce7b0e95509.json
+gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509.json
 
 gcloud --quiet config set project deep-stock-268818
 
-gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-backend
-gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-frontend
+gcloud docker -- push gcr.io/deep-stock-268818/appengine/deep-stock-backend
+gcloud docker -- push gcr.io/deep-stock-268818/appengine/deep-stock-frontend
 
 # yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
 # yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
