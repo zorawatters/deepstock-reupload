8bf4192b82f5cccdc4cae090aecf458c0c4f1a90
JakeAndrus <thejakeandrus@gmail.com>
2020-04-01 22:52:23 -0600

    app yaml again

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,19 +4,19 @@ echo "We're runing deploy prod"
 
 set -e
 
-docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
-docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
+# docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
+# docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
 
 gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509.json
 
-gcloud --quiet config set project deep-stock-268818
+# gcloud --quiet config set project deep-stock-268818
 
+ls
 gcloud app deploy /frontend/app.yaml
 gcould app deploy /backend/app.yaml
 
-# gcloud docker -- push us.gcr.io/deep-stock-268818/appengine
-gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
-gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
+# gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
+# gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
 
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
