dd4e0185ba96102241e24ce3e4287b8e4bd0d403
JakeAndrus <thejakeandrus@gmail.com>
2020-04-01 22:56:15 -0600

    find it bastard

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -12,8 +12,8 @@ gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509
 # gcloud --quiet config set project deep-stock-268818
 
 ls
-gcloud app deploy /frontend/app.yaml
-gcould app deploy /backend/app.yaml
+gcloud app deploy frontend/app.yaml
+gcould app deploy backend/app.yaml
 
 # gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
 # gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
