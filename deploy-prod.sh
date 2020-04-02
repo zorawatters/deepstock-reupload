d40c77f50b02bf90d57c5ae15b06133c4dc6b16b
JakeAndrus <thejakeandrus@gmail.com>
2020-04-01 21:52:35 -0600

    dumb idot forogt the ting

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -12,8 +12,8 @@ gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509
 gcloud --quiet config set project deep-stock-268818
 
 # gcloud docker -- push us.gcr.io/deep-stock-268818/appengine
-gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-frontend
-gcloud docker -- push gcr.io/deep-stock-268818/deep-stock-backend
+gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
+gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
 
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
