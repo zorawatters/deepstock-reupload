edb8f77db80e4a24bce8ce2ae7c7ccab9bc7969b
JakeAndrus <thejakeandrus@gmail.com>
2020-04-02 18:25:18 -0600

    both also delete

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -9,7 +9,7 @@ gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509
 
 gcloud --quiet config set project deep-stock-268818
 
-# gcloud app versions list | grep -v SERVING | awk '{print $2}' | tail -n +1 | xargs -I {} gcloud app versions delete {}
+gcloud app versions list | grep -v SERVING | awk '{print $2}' | tail -n +1 | xargs -I {} gcloud app versions delete {}
 gcloud app deploy frontend/ --stop-previous-version
 gcloud app deploy backend/ --stop-previous-version
 
