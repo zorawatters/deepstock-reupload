8e939b64bd5880c6ee9299b419902321bfeb6d02
JakeAndrus <thejakeandrus@gmail.com>
2020-04-03 17:06:47 -0600

    change gunicorn config

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,7 +6,7 @@ set -e
 # technically unnec
 docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
 docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
-docker build -t us.gcr.io/deep-stock-268818/deep-stock-nginx:latest -f nginx/Dockerfile ./nginx
+# docker build -t us.gcr.io/deep-stock-268818/deep-stock-nginx:latest -f nginx/Dockerfile ./nginx
 
 
 gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509.json
@@ -18,12 +18,12 @@ gcloud --quiet config set project deep-stock-268818
 # gcloud app versions list | grep -v SERVING | awk '{print $2}' | tail -n +1 | xargs -I {} gcloud app versions delete {}
 gcloud app deploy frontend/ --stop-previous-version
 gcloud app deploy backend/ --stop-previous-version
-gcloud app deploy nginx/ --stop-previous-version
+# gcloud app deploy nginx/ --stop-previous-version
 
 # send images to gcr
 gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
 gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
-gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-nginx
+# gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-nginx
 
 # add tags?
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
