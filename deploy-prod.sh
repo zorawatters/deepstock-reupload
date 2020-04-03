8082106ff0fb1f84ee3ccc6300e784bd07a3236c
JakeAndrus <thejakeandrus@gmail.com>
2020-04-02 20:31:01 -0600

    hopefully final travis app engine deployment stuff

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
-# docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
+# build images
+# technically unnec
+docker build -t us.gcr.io/deep-stock-268818/deep-stock-backend:latest -f backend/Dockerfile ./backend
+docker build -t us.gcr.io/deep-stock-268818/deep-stock-frontend:latest -f frontend/Dockerfile ./frontend
 
 gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509.json
 
@@ -13,8 +15,10 @@ gcloud --quiet config set project deep-stock-268818
 gcloud app deploy frontend/ --stop-previous-version
 gcloud app deploy backend/ --stop-previous-version
 
-# gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
-# gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
+# send images to gcr
+gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
+gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-backend
 
+# add tags?
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
 # yes | gcloud beta container images add-tag us.gcr.io/deep-stock-268818/deep-stock-backend:latest gcr.io/${PROJECT_PROD}/${NODE_IMAGE}:latest
