338d24749053b08a22827974affbeaf9e18092be
JakeAndrus <thejakeandrus@gmail.com>
2020-04-02 18:24:50 -0600

    both

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -10,7 +10,7 @@ gcloud auth activate-service-account --key-file ./deep-stock-268818-fce7b0e95509
 gcloud --quiet config set project deep-stock-268818
 
 # gcloud app versions list | grep -v SERVING | awk '{print $2}' | tail -n +1 | xargs -I {} gcloud app versions delete {}
-# gcloud app deploy frontend/ --stop-previous-version
+gcloud app deploy frontend/ --stop-previous-version
 gcloud app deploy backend/ --stop-previous-version
 
 # gcloud docker -- push us.gcr.io/deep-stock-268818/deep-stock-frontend
