a1806f49c6a50138903188560587990065f02b74
Jake Andrus <thejakeandrus@gmail.com>
2020-04-06 23:35:16 +0000

    example of api call in views/Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <b-row class="shadow-lg p-3 mb-5 rounded col-sm">
-      <main-card title="Stock" custom ="card shadow col-lg-8"><stock-card custom="bg-primary"></stock-card></main-card>
+      <main-card :title="hello_stocks" custom ="card shadow col-lg-8"><stock-card custom="bg-primary"></stock-card></main-card>
       <main-card title="Toggle" custom ="card shadow col-lg-4"><toggle-card custom = "bg-warning"></toggle-card></main-card>
       <main-card title="Twitter Card" custom ="card shadow col-lg-12"><twitter-card custom = "bg-secondary"></twitter-card></main-card>
     </b-row>
@@ -22,5 +22,13 @@ export default {
     'twitter-card': TwitterCard,
     'toggle-card': ToggleCard
   },
+  data(){
+    return {
+      hello_stocks: ''
+    }
+  },
+  async mounted(){
+    this.hello_stocks = (await this.$http.get(this.$backendUrl + '/message')).data
+  }
 }
 </script>
