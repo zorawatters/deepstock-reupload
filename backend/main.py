380e98c1334de25420b8fc3b87173e1f518c72cd
kylefaucher <kyfa3283@colorado.edu>
2020-04-08 20:58:52 +0000

    updating backend files

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,99 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_cors import CORS
+from alpha_vantage.timeseries import TimeSeries
+from flask import request
+from flask_pymongo import pymongo
+from pymongo import MongoClient
 import yfinance as yf
+from alpha_vantage.timeseries import TimeSeries
+import json
 import tweepy
 from tweepy.parsers import JSONParser
 from tweepy.streaming import StreamListener
-import json
+from flask_jsonpify import jsonpify
+# from dotenv import Dotenv
+# dotenv = Dotenv('./.env')
+# print(dotenv)
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app = Flask(__name__)
 
+CONNECTION_STRING = "mongodb://deep-stock-cu:deep2020stock@deep-stock-cluster-shard-00-00-bwk5a.gcp.mongodb.net:27017,deep-stock-cluster-shard-00-01-bwk5a.gcp.mongodb.net:27017,deep-stock-cluster-shard-00-02-bwk5a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=deep-stock-cluster-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('deep-stock')
+collection = db['companies']
+
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+@app.route('/message')
+def get_message():
+  return 'Hello Stonks'
+
+@app.route('/stock_data')
+def get_stock_data():
+	msft = yf.Ticker("MSFT")
+
+	dataframe = msft.history(period="1")
+	print(dataframe)
+	#dataframe['Date'] = dataframe['Date'].dt.strftime('%Y-%m-%d')
+	return dataframe.reset_index().to_json(orient='records', date_format='iso')
+
+
+@app.route("/<string:company>/tweets", methods=['PUT'])
+def addTweet(company):
+    new_tweet = request.get_json()
+    collection.update({"ticker" : company}, {'$push': {'tweets': new_tweet}})
+
+
+'''
+  Storing stock data api
+  Gets json data from response body and stores it into database
+'''
+
+@app.route('/users/store_data', methods=['POST'])
+def insert_document():
+    req_data = request.get_json()
+
+    collection.insert_one(req_data).inserted_id
+    return ('', 204)
+
+
+'''
+  Diplaying stock data api
+  Gets all the sotored data in the database and retruns a json file
+'''
+
+
+@app.route('/<string:company>/intraday', methods=['GET'])
+def get_intraday(company):
+
+
+    key = 'CYNCL6X4FUN4SE0K'
+
+    ts = TimeSeries(key= key)
+
+    intraday_data, data_info = ts.get_intraday(symbol=company, outputsize='compact', interval='5min')
+
+    return json.dumps(intraday_data)
+
+
+
+@app.route('/users/display_data')
+def get_documents():
+
+    documents = collection.find()
+
+    response = []
+
+    for doc in documents:
+        doc['_id'] = str(doc['_id'])
+        response.append(doc)
+
+    return json.dumps(response)
 
 @app.route('/tweepy')
 def get_tweepy():
@@ -23,17 +108,12 @@ def get_tweepy():
 	
 	search = request.args.get('q')
 	print(search)
-	public_tweets = api.user_timeline(search)
-	print(public_tweets)
+	public_tweets = api.search(search, count = 1)
+	print(json.dumps(public_tweets))
 	return json.dumps(public_tweets)
 
-@app.route('/message')
-def get_message():
-  return 'Hello Stonks'
+if os.getenv('environment') == 'dev':
+    app.run(host='0.0.0.0')
+elif __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
-@app.route('/stock_data')
-def get_stock_data():
-	msft = yf.Ticker("MSFT")
-	print(msft.info)
-
-app.run(host='0.0.0.0')
--- backend/setup-server.sh
+++ backend/setup-server.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-pip3 install -r requirements.txt
 python3 -m venv venv
 source venv/bin/activate
+pip3 install -r requirements.txt
 gunicorn main:app
