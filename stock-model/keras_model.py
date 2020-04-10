2f935a74bc8fda90c21b0a7d538048527e9f84f5
Jake Andrus <thejakeandrus@gmail.com>
2020-04-10 02:59:54 +0000

    added some mlflow things

--- stock-model/keras_model.py
+++ stock-model/keras_model.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
 
-def build(T, D):	
+def build_model(T, D):	
 	i = Input(shape=(T, D))
 	x = LSTM(50)(i)
 	x = Dense(1, activation='sigmoid')(x)
@@ -12,3 +12,9 @@ def build(T, D):
 	model.compile(loss='binary_crossentropy', optimizer=Adam(lr=.001), metrics=['accuracy'])
 
 	return model
+
+def build_dataset(features, labels, shuffle, num_epochs, batch_size):
+	dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+	dataset = dataset.shuffle(buffer_size=len(features))
+	dataset = dataset.repeat(num_epochs).batch(batch_size)
+	return dataset
--- stock-model/train.py
+++ stock-model/train.py
@@ -1,44 +1,63 @@
-import build_model
+import keras_model
+import argparse
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+import mlflow.tensorflow
+from builtins import int
+mlflow.tensorflow.autolog()
 
-df = pd.read_csv('https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv')
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--ticker', default='')
+	parser.add_argument('--num-epochs', type=int, default='10')
+	parser.add_argument('--batch-size', type=int, default='64')
+	args, _ = parser.parse_known_args()
+	return args
 
-df['PrevClose'] = df['close'].shift(1)
+def train_and_evaluate(args):
+	df = pd.read_csv('https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv')
 
-df['Return'] = (df['close'] - df['PrevClose']) / df['PrevClose']
+	df['PrevClose'] = df['close'].shift(1)
 
-input_data = df[['open', 'high', 'low', 'close', 'volume']].values
-targets = df['Return'].values
+	df['Return'] = (df['close'] - df['PrevClose']) / df['PrevClose']
 
-T = 10
-D = input_data.shape[1]
-N = len(input_data) - T
+	input_data = df[['open', 'high', 'low', 'close', 'volume']].values
+	targets = df['Return'].values
 
-Ntrain = len(input_data) * 2 // 3
-scaler = StandardScaler()
-scaler.fit(input_data[:Ntrain + T - 1])
-input_data = scaler.transform(input_data)
+	T = 10
+	D = input_data.shape[1]
+	N = len(input_data) - T
 
-x_train = np.zeros((Ntrain, T, D))
-y_train = np.zeros(Ntrain)
+	Ntrain = len(input_data) * 2 // 3
+	scaler = StandardScaler()
+	scaler.fit(input_data[:Ntrain + T - 1])
+	input_data = scaler.transform(input_data)
 
-for t in range(Ntrain):
-	x_train[t, :, :] = input_data[t:t+T]
-	y_train[t] = (targets[t+T] > 0)
+	x_train = np.zeros((Ntrain, T, D))
+	y_train = np.zeros(Ntrain)
 
-x_test = np.zeros((N - Ntrain, T, D))
-y_test = np.zeros(N - Ntrain)
+	for t in range(Ntrain):
+		x_train[t, :, :] = input_data[t:t+T]
+		y_train[t] = (targets[t+T] > 0)
 
-for u in range(N - Ntrain):
-  # u counts from 0...(N - Ntrain)
-  # t counts from Ntrain...N
-  t = u + Ntrain
-  x_test[u, :, :] = input_data[t:t+T]
-  y_test[u] = (targets[t+T] > 0)
+	x_test = np.zeros((N - Ntrain, T, D))
+	y_test = np.zeros(N - Ntrain)
 
-model = build_model.build(T, D)
-print(model.summary())
+	for u in range(N - Ntrain):
+	  # u counts from 0...(N - Ntrain)
+	  # t counts from Ntrain...N
+	  t = u + Ntrain
+	  x_test[u, :, :] = input_data[t:t+T]
+	  y_test[u] = (targets[t+T] > 0)
 
-r = model.fit(x_train, y_train, batch_size=32, epochs=300, validation_data=(x_test, y_test))
+	model = keras_model.build_model(T, D)
+	print(model.summary())
+
+	r = model.fit(x_train, y_train, batch_size=args.batch_size, epochs=args.num_epochs, validation_data=(x_test, y_test))
+
+if __name__ == '__main__':
+	args = get_args()
+	tf.compat.v1.logging.set_verbosity('DEBUG')
+	train_and_evaluate(args)
