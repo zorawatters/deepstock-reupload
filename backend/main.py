534b665b25c4442ce72d1619d1e83b73910c2b6f
kylefaucher <kyfa3283@colorado.edu>
2020-04-15 20:52:28 +0000

    added twitter.py from zora's branch

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ import json
 import tweepy
 from tweepy.parsers import JSONParser
 from tweepy.streaming import StreamListener
+from twitter import TwitterClient
 from flask_jsonpify import jsonpify
 # from dotenv import Dotenv
 # dotenv = Dotenv('./.env')
@@ -95,32 +96,85 @@ def get_documents():
 
     return json.dumps(response)
 
-@app.route('/tweepy')
+# @app.route('/tweepy')
+# def get_tweepy():
+#     api_secret_key = '4n84JSQuX4qnZFt1jBVYOSbl8eA6mvOwSMbbtOjlpYX1cQQ6y0'
+#     api_key = 'HFozqhWZcH3P2cUXsyBSZrf6P'
+#     access_token = '341961921-uZrcljPNobyBflSXuZSYrnNZZLZ5r37FZbE6gmMX'
+#     access_token_secret = 'fXKPY5rQLbXYTRCYVJPsDUEBqCr5lt3wKZiMBbE4F2i8W'
+#     auth = tweepy.OAuthHandler(api_key, api_secret_key)
+#     auth.set_access_token(access_token, access_token_secret)
+#     api = tweepy.API(auth, parser = JSONParser())
+#     tweets = []
+#     f = open('companies.json',) 
+#     data = json.load(f) 
+#     for i in data['companies']:
+#         # make call to tweepy api here and store in mongodb for each company
+#         public_tweets = api.search(i, count = 3)
+#         for tweet in public_tweets['statuses']:
+#             tweet_json_string = {"ticker": '', "text": '', "id": ''}
+#             tweet_json_dump = json.dumps(tweet_json_string)
+#             tweet_json_object = json.loads(tweet_json_dump)
+#             tweet_json_object["ticker"] = i;
+#             tweet_json_object["text"] = tweet["text"];
+#             tweet_json_object["id"] = tweet["id_str"];
+#             tweets.append(tweet_json_object)
+          
+#     f.close() 
+
+#     return json.dumps(tweets)
+
+@app.route('/tweepy', methods=['GET'])
 def get_tweepy():
-	api_secret_key = '4n84JSQuX4qnZFt1jBVYOSbl8eA6mvOwSMbbtOjlpYX1cQQ6y0'
-	api_key = 'HFozqhWZcH3P2cUXsyBSZrf6P'
-	access_token = '341961921-uZrcljPNobyBflSXuZSYrnNZZLZ5r37FZbE6gmMX'
-	access_token_secret = 'fXKPY5rQLbXYTRCYVJPsDUEBqCr5lt3wKZiMBbE4F2i8W'
+    # api_secret_key = '4n84JSQuX4qnZFt1jBVYOSbl8eA6mvOwSMbbtOjlpYX1cQQ6y0'
+    # api_key = 'HFozqhWZcH3P2cUXsyBSZrf6P'
+    # access_token = '341961921-uZrcljPNobyBflSXuZSYrnNZZLZ5r37FZbE6gmMX'
+    # access_token_secret = 'fXKPY5rQLbXYTRCYVJPsDUEBqCr5lt3wKZiMBbE4F2i8W'
 
-	auth = tweepy.OAuthHandler(api_key, api_secret_key)
-	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth, parser = JSONParser())
-	
-	search = request.args.get('q')
-	print(search)
-	public_tweets = api.search(search, count = 1)
-	print(json.dumps(public_tweets))
-	return json.dumps(public_tweets)
+    # auth = tweepy.OAuthHandler(api_key, api_secret_key)
+    # auth.set_access_token(access_token, access_token_secret)
+  # api = tweepy.API(auth, parser = JSONParser())
 
-@app.route('/all_data', methods=['GET'])
-def get_all_data():
-  all_data = collection.find()
-  response = []
-  for x in all_data:
-    x['_id'] = str(x['_id'])
-    response.append(x)
+  # # search = request.args.get('q')
+  # # print(search)
+  # # public_tweets = api.user_timeline(search)
+  # # print(public_tweets)
+  # #creating a stream of tweets through tweepy
+  # tweetStreamListener = TweetListener()
+  # tweetStream = tweepy.Stream(auth = auth, listener = tweetStreamListener)
+  # tweetStream.filter(track = ['tesla stocks']) #looking for tweets about tesla
+  # return "got it"
+  # creating object of TwitterClient Class 
+  print("b4")
+  api = TwitterClient()
+  print("got api")
+  # calling function to get tweets 
+  tweets = api.get_tweets(query = 'TSLA', count = 200) 
 
-  return json.dumps(response)
+  # picking positive tweets from tweets 
+  ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
+  # percentage of positive tweets 
+  print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
+  # picking negative tweets from tweets 
+  ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
+  # percentage of negative tweets 
+  print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+  # percentage of neutral tweets 
+ # print("Neutral tweets percentage: {} %".format(100*len(tweets - ntweets - ptweets)/len(tweets))) 
+
+  # printing first 5 positive tweets 
+  print("\n\nPositive tweets:") 
+  for tweet in ptweets[:10]: 
+    print(tweet['text']) 
+
+  #  printing first 5 negative tweets 
+  print("\n\nNegative tweets:") 
+  for tweet in ntweets[:10]: 
+    print(tweet['text']) 
+
+  # add to mongodb 
+
+  return ntweets[0]
 
 
 if os.getenv('environment') == 'dev':
