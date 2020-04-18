755163db7d72ab796f8994ad3ac65459e704daa8
kylefaucher <kyfa3283@colorado.edu>
2020-04-18 20:57:40 +0000

    don't need these files anymore

--- backend/gettweets.py
+++
@@ -1,9 +0,0 @@
-import json
-
-f = open('companies.json',) 
-data = json.load(f) 
-for i in data['companies']: 
-	# make call to tweepy api here and store in mongodb for each company
-    print(i) 
-  
-f.close() 
