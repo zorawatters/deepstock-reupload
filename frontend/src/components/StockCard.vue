42586ca66e2f168d4b6514ce86e65fc4b822ed5f
Jake Andrus <thejakeandrus@gmail.com>
2020-04-19 00:34:17 +0000

    fusioncharts time series working

--- frontend/src/components/StockCard.vue
+++ frontend/src/components/StockCard.vue
@@ -1,47 +1,97 @@
 <template>
-<div :class = "custom" id="chart-container">
-      <fusioncharts
-      :type="type"
-      :width="width"
-      :height="height"
-      :dataformat="dataFormat"
-      :dataSource="dataSource"
-      >
-      </fusioncharts>
+<div :class = "custom">
+  <fusioncharts
+  :type="type"
+  :width="width"
+  :height="height"
+  :dataformat="dataFormat"
+  :dataSource="dataSource"
+  ></fusioncharts>
 </div>
 </template>
 <script>
-
-
-// STEP 3: Configure your chart
-const dataSource = {
-  chart: {
-    caption: "Intraday movements",
-    subcaption: "In MMbbl = One Million barrels",
-    xaxisname: "Country",
-    yaxisname: "Reserves (MMbbl)",
-    numbersuffix: "K",
-    theme: "fusion"
-  },
-  data: chartData
-};
-
+const d = [
+  ['2020-04-17 10:05:00', 700],
+  ['2020-04-17 13:25:00', 500],
+  ['2020-04-17 14:30:00', 600],
+  ['2020-04-17 15:30:00', 740]
+]
 export default {
   name: 'stock-card',
   props: {
     custom: {
       type: String,
       default: ''
+    },
+    ticker: {
+      type: String,
+      default: 'TSLA'
     }
   },
   data() {
     return {
-      "type": "column2d",
-      "renderAt": "chart-container",
-      "width": "550",
-      "height": "350",
-      "dataFormat": "json",
-      dataSource
+      type: "timeseries",
+      width: "550",
+      height: "350",
+      dataFormat: "json",
+      chartData: [],
+      dataStore: new FusionCharts.DataStore(),
+      schema: [{
+        name: "Time",
+        type: "date",
+        format: "%Y-%m-%d %-H:%M:%S"
+      }, {
+        name: "Price",
+        type: "number"
+      }]
+    }
+  },
+  async mounted(){
+    this.getIntraday()
+    setInterval(this.getIntraday, 10000)
+  },
+  methods:{
+    getIntraday: async function(){
+      var response = await this.$http.get(this.$backendUrl + '/' + this.ticker + '/intraday')
+      var data = response.data
+      this.chartData = []
+
+      for(var datetime in data){
+        this.chartData.push([datetime, Number(data[datetime][0]['4. close'])])
+
+      }
+    }
+  },
+  computed:{
+    dataSource: function(){
+      
+      var r =  {
+        chart: {
+          caption: "Intraday movements of " + this.ticker,
+          //subcaption: "In MMbbl = One Million barrels",
+          xaxisname: "Time",
+          yaxis: [
+            {
+              plot: [
+                {
+                  value: "Price",
+                  type: 'line',
+                  style: {
+                    plot: {
+                      "stroke-dasharray": "5 2"
+                    }
+                  }
+                }
+              ]
+            }
+          ],
+          //numbersuffix: "K",
+          theme: "fusion"
+        },
+        data: this.dataStore.createDataTable(this.chartData, this.schema)
+      }
+      console.log(r)
+      return r
     }
   }
 }
