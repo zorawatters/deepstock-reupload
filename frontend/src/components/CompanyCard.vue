cc219755447c4850cc633e7caa08ecde288c90eb
keaton <bsemailme@gmail.com>
2020-04-22 14:54:37 -0600

    added netnaels photo and update CompanyCard.vue

--- frontend/src/components/CompanyCard.vue
+++ frontend/src/components/CompanyCard.vue
@@ -4,37 +4,37 @@
     <b-row>
       <div class = "col-12 text-left list-group list-group-horizontal">
         <b-row class="">
-        <dl class="col-6 pt-5">
+        <dl class="col-6 pt-4">
           <ul>
-            <li class="list-group-item"><span class="text-left "><b>Employees</b> <span style="color:#42b983" class="">{{metadata.fullTimeEmployees }}</span></span></li>
+            <li class="list-group-item"><span class="text-left "><b>Employees</b> <span style="color:#42b983" class=""> {{metadata.fullTimeEmployees }} </span></span></li>
             <!-- Use this one ^^-->
-            <!-- <div class="result"> {{metadata.fullTimeEmployees }} </div></div> -->
+            <!-- <div class="result"> 48016 </div></div> -->
 
-            <li class="list-group-item"><span class="text-left "><b>Headquarters</b> <span style="color:#42b983" class="">{{metadata.city}}, {{metadata.state}} </span></span></li>
+            <li class="list-group-item"><span class="text-left "><b>Headquarters</b> <span style="color:#42b983" class=""> {{metadata.city}}, {{metadata.state}} </span></span></li>
             <!-- Use this one ^^-->
-            <!-- <div class="result"> {{metadata.city}}, {{metadata.state}} </div></div> -->
+            <!-- <div class="result">Palo Alto, CA </div></div> -->
 
 
             <li class="list-group-item"><span class="text-left "><b>Market Cap</b> <span style="color:#42b983" class="">{{fundamentals.marketCap.fmt }}</span></span></li>
             <!-- Use this one ^^-->
-            <!-- <div class="result"> {{fundamentals.marketCap.fmt }} </div></div> -->
+            <!-- <div class="result"> 126.62B </div></div> -->
           </ul>
           </dl>
-          <dl class="col-6 pt-5">
+          <dl class="col-6 pt-4">
             <ul>
-            <li class="list-group-item"><span class="text-left "><b>Average Volume</b> <span style="color:#42b983" class="">{{fundamentals.averageVolume.fmt }} </span></span></li>
+            <li class="list-group-item"><span class="text-left "><b>Average Volume</b> <span style="color:#42b983" class=""> {{fundamentals.averageVolume.fmt }}</span></span></li>
             <!-- Use this one ^^-->
-            <!-- <div class="result"> {{fundamentals.averageVolume.fmt }} </div></div> -->
+            <!-- <div class="result"> 20.72M </div></div> -->
 
             <li class="list-group-item"><span class="text-left "><b>52 Week High</b> <span style="color:#42b983" class="">{{fundamentals.fiftyTwoWeekHigh.fmt }}</span></span></li>
 
           <!-- Use this one ^^-->
-          <!-- <div class="result"> {{fundamentals.fiftyTwoWeekHigh.fmt }} </div></div> -->
+          <!-- <div class="result"> 968.99 </div></div> -->
 
           <li class="list-group-item"><span class="text-left "><b>52 Week Low</b> <span style="color:#42b983" class="">{{fundamentals.fiftyTwoWeekLow.fmt }}</span></span></li>
 
           <!-- Use this one ^^-->
-          <!-- <div class="result"> {{fundamentals.fiftyTwoWeekLow.fmt }} </div></div> -->
+          <!-- <div class="result"> 176.99 </div></div> -->
         </ul>
         </dl>
       </b-row>
@@ -46,7 +46,7 @@
           {{metadata.longBusinessSummary }}
         </p>
         <!-- Use this one ^^-->
-        <!-- <div class="section-description">{{metadata.longBusinessSummary }} </div> -->
+        <!-- <div class="section-description"> Tesla, Inc. designs, develops, manufactures, leases, and sells electric vehicles, and energy generation and storage systems in the United States, China, Netherlands, Norway, and internationally. The company operates in two segments, Automotive; and Energy Generation and Storage. The Automotive segment offers sedans and sport utility vehicles. It also provides electric powertrain components and systems; and services for electric vehicles through its company-owned service locations, and Tesla mobile service technicians, as well as sells used vehicles. This segment markets and sells its products through a network of company-owned stores and galleries, as well as through its own Website. The Energy Generation and Storage segment offers energy storage products, such as rechargeable lithium-ion battery systems for use in homes, industrial, commercial facilities, and utility grids; and designs, manufactures, installs, maintains, leases, and sells solar energy generation and energy storage products to residential and commercial customers. It also provides vehicle insurance services, as well as renewable energy. The company was formerly known as Tesla Motors, Inc. and changed its name to Tesla, Inc. in February 2017. Tesla, Inc. was founded in 2003 and is headquartered in Palo Alto, California.</div> -->
       </div>
     </b-row>
 
