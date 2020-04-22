4320182380187d538e8ba09b9f6891d2d59e47a7
kylefaucher <kyfa3283@colorado.edu>
2020-04-22 19:30:54 +0000

    twitter card

--- frontend/src/components/TwitterCard.vue
+++ frontend/src/components/TwitterCard.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="card text-center">
 
-      <div class="card-body">
-        <h5 class="card-title">INSERT TITLE HERE</h5>
-        <p class="card-text">INSERT TWEET HERE</p>
-        <a href="#" class="btn btn-info">Link to tweet/bio?</a>
+      <div class="card-body twitter-card-body" style = "max-height: 250px; overflow-y: scroll;">
+        <div class = "tweet-card-tweet" style = "background: #f1f1f1; padding: 10px; border-radius: 5px;margin-bottom: 15px;box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);" v-for="tweet in tweets" :key="tweet">
+          {{ tweet }}
+        </div>
       </div>
       <div class="card-footer text-muted">
-        {{ tweets ? tweets[0] : ''}}
+       Recent {{ticker}} Tweets
       </div>
     </div>
   </div>
@@ -32,6 +32,6 @@ export default {
     return {
     }
   },
-  computed: mapGetters({tweets: 'getTweetText'})
+  computed: mapGetters({tweets: 'getTweetText', ticker: 'getTicker'})
 }
 </script>
